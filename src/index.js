98bb930fed49a9f23dfd0877ac27012e5d1f4f7b
Kamil Wronka <kamilwronka7@gmail.com>
2018-03-12 15:35:44 +0100

    checking

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
         <div>
+            Checking something
             <Route path="/" component={PostsIndex} />
         </div>
     </BrowserRouter>
