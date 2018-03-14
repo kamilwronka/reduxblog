71244044a00c77d9baf2f60ce94bb5b1be9f8918
Kamil Wronka <kamilwronka7@gmail.com>
2018-03-14 13:18:05 +0100

    add existing file

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from "../actions";
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta } = field;
+        const className = `form-group ${ meta.touched && meta.error ? 'has-danger' : ''}`;
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
-                {field.meta.error}
+                <div className="text-help">
+                    {meta.touched ? meta.error : ''}
+                </div>
             </div>
         );
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
+
     }
 
     render() {
@@ -43,6 +54,7 @@ class PostsNew extends Component {
                 <button type="submit" className="btn btn-primary">
                     Submit
                 </button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     };
@@ -69,4 +81,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
